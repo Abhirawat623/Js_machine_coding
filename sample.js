12eaf9db438c60ed8f8808389e7ce7402ca482a8
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-12-19 16:39:50 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,31 @@
+// let coarse = [ "html" , "css","js", "react" ];
+// console.log(coarse);
+// let rem = coarse.pop();
+// console.log(rem)
+// rem = coarse.pop();
+// console.log(rem)
+
+// let x =5;
+// let y=756;
+// let z=56;
+
+// if(x<y &&x<z){
+//     console.log(x)
+// }
+// else if(y<x && y<z){
+//     console.log(y)
+// }
+// else{
+//     console.log(z)
+// }
+
+let a=23;
+let b=32;
+let c=21;
+
+if(a+b<c && b+c<a && c+a<b){
+    console.log('traingle');
+}
+else{
+    console.log('not traingle')
+}
